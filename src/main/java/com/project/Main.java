a9a1a158cf3578c1174c2e210774f341fcb8b572
Fanantenana NyAina <hei.fanantenana@gmail.com>
2025-11-24 18:25:14 +0300

    feat: main class to test

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -1,17 +1,29 @@
 package com.project;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.project.entity.Purchase;
+import com.project.entity.methode.CashPayment;
+import com.project.entity.methode.ChequePayment;
+import com.project.entity.methode.MobileMoneyPayment;
+import com.project.entity.methode.VisaPayment;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        VisaPayment visaPayment = new VisaPayment(2_000.0d);
+        CashPayment cashPayment = new CashPayment(20_000.0d);
+        ChequePayment chequePayment = new ChequePayment();
+        MobileMoneyPayment mobileMoneyPayment = new MobileMoneyPayment(1_000.0d);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        Purchase purchaseWithVisa = new Purchase(visaPayment, 10_000.0d);
+        System.out.println("purchaseWithVisaTotal : "+ purchaseWithVisa.processPayment());
+
+        Purchase purchaseWithCash = new Purchase(cashPayment, 10_000.0d);
+        System.out.println("purchaseWithCashTotal : "+ purchaseWithCash.processPayment());
+
+        Purchase purchaseWithCheque = new Purchase(chequePayment, 10_000.0d);
+        System.out.println("purchaseWithChequeTotal : "+ purchaseWithCheque.processPayment());
+
+        Purchase purchaseWithMobileMoney = new Purchase(mobileMoneyPayment, 10_000.0d);
+        System.out.println("purchaseWithMobileMoneyTotal : "+ purchaseWithMobileMoney.processPayment());
     }
 }
